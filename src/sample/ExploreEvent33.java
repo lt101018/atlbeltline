ccc946d8fb8d412ddc49106c3fe2eef3273d65a0
Teng Li <tengli@gatech.edu>
2019-04-21 17:43:51 -0400

    35

--- src/sample/ExploreEvent33.java
+++ src/sample/ExploreEvent33.java
@@ -84,33 +84,58 @@ public class ExploreEvent33 {
 
     public void btnFilter(ActionEvent actionEvent) throws SQLException {
 
-        table.getItems().clear();
-        String sql = "create or replace view eventtotalvisit as\n" +
-                "select eventname,sitename,eventstartdate,count(*) as totalvisit\n" +
-                "from visit_event\n" +
-                "group by eventname,sitename,eventstartdate;";
+        String dateTableSql1 = "";
+        if(datepicker1.getValue()!=null && datepicker2.getValue()!=null){
+            String formattedDate1 = datepicker1.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            String formattedDate2 = datepicker2.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            dateTableSql1 = " and (v.visitdate between '"+formattedDate1+"'and '"+formattedDate2+"') ";
+        }
 
+        String dateTableSql2 = "";
+        if(datepicker1.getValue()!=null && datepicker2.getValue()!=null){
+            String formattedDate1 = datepicker1.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            String formattedDate2 = datepicker2.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            dateTableSql2 = " and (v.visitdate between '"+formattedDate1+"'and '"+formattedDate2+"') ";
+        }
+
+
+        table.getItems().clear();
+        String sql = "CREATE   or replace view eventtotalvisit AS\n" +
+                "SELECT e.name,e.sitename,e.price,e.startdate,count(case when e.name=v.eventname and e.sitename=v.sitename and e.startdate=v.eventstartdate "+dateTableSql1+" then 1 end) as totalvisit\n" +
+                "from event as e\n" +
+                "left join visit_event as v\n" +
+                "on e.name=v.eventname and e.sitename=v.sitename and e.startdate=v.eventstartdate\n" +
+                "group by e.name,e.sitename,e.startdate;";
+        System.out.println(sql);
         Statement statement = conn.createStatement();
         statement.executeUpdate(sql);
+        sql = "CREATE   or replace view eventmyvisit AS\n" +
+                "SELECT e.name,e.sitename,e.price,e.startdate,count(case when e.name=v.eventname and e.sitename=v.sitename and e.startdate=v.eventstartdate and v.visitorusername='"+UserInfo.username+"' "+dateTableSql2+" then 1 end) as myvisit\n" +
+                "from event as e\n" +
+                "left join visit_event as v\n" +
+                "on e.name=v.eventname and e.sitename=v.sitename and e.startdate=v.eventstartdate\n" +
+                "group by e.name,e.sitename,e.startdate;";
+        statement.executeUpdate(sql);
+        System.out.println(sql);
 
-        String eventNameSql = "";
+        String eventNameSql = " 1=1 ";
         if(tfname.getText().length()!=0)
-            eventNameSql = "and e.name like '%"+tfname.getText()+"%'";
+            eventNameSql = " e.name like '%"+tfname.getText()+"%' ";
 
         String descriptionSql = "";
         if(tfKeyword.getText().length()!=0)
-            descriptionSql = "and  e.description like '%"+tfKeyword.getText()+"%'";
+            descriptionSql = " and  e.description like '%"+tfKeyword.getText()+"%' ";
 
         String dateSql1 = "";
         if(datepicker1.getValue()!=null && datepicker2.getValue()!=null){
             String formattedDate1 = datepicker1.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
             String formattedDate2 = datepicker2.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-            dateSql1 = "and e.startdate>='"+formattedDate1+"' and e.enddate<='"+formattedDate2+"'";
+            dateSql1 = " and not (e.startdate>'"+formattedDate2+"' or e.enddate<'"+formattedDate1+"') ";
         }
 
         String siteSql = "";
         if(!cbSiteName.getValue().toString().equals("ALL")){
-            siteSql = "and e.sitename='"+cbSiteName.getValue().toString()+"'\n";
+            siteSql = " and e.sitename='"+cbSiteName.getValue().toString()+"' ";
         }
 
         String visitL = "0", visitR = Integer.MAX_VALUE+"";
@@ -125,42 +150,26 @@ public class ExploreEvent33 {
             priceR = tfPriceRange2.getText();
 
         String soldOutSql = "";
-        if(checkSoldOut.isSelected())
-            soldOutSql = "and (e.capacity-etv.totalvisit)!='0'";
+        if(!checkSoldOut.isSelected())
+            soldOutSql = " and (e.capacity-etv.totalvisit)!='0' ";
+
+        String myVisitSql = "";
+        if(!checkVisited.isSelected())
+            myVisitSql = " and emv.myvisit ='0' " ;
 
 
-        String eventNameSql1 = "where 1=1";
-        if(tfname.getText().length()!=0)
-            eventNameSql1 = "where ev.name like '%"+tfname.getText()+"%'";
 
-        String descriptionSql1 = "";
-        if(tfKeyword.getText().length()!=0)
-            descriptionSql1 = "and  ev.description like '%"+tfKeyword.getText()+"%'";
-
-        String dateSql11 = "";
-        if(datepicker1.getValue()!=null && datepicker2.getValue()!=null){
-            String formattedDate1 = datepicker1.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-            String formattedDate2 = datepicker2.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-            dateSql11 = "and ev.startdate>='"+formattedDate1+"' and ev.enddate<='"+formattedDate2+"'";
-        }
-
-        sql = "select e.name, e.sitename,e.price,(e.capacity-etv.totalvisit) as ticket_remaining,etv.totalvisit, count(*)as myvisits, e.startdate, e.enddate, e.description\n" +
-                "from event as e, visit_event as v,eventtotalvisit as etv\n" +
-                "where e.name =v.eventname\n" +
-                "and e.sitename=v.sitename \n" +
-                "and e.startdate=v.eventstartdate\n" +
-                eventNameSql+" "+descriptionSql+"  "+dateSql1 +" "+siteSql+
-                "and etv.totalvisit>='"+visitL+"' and etv.totalvisit<='"+visitR+"'\n" +
-                "and e.price>='"+priceL+"'and e.price<='"+priceR+"' " + soldOutSql+" "+
-                "and e.name =v.eventname\n" +
-                "and e.sitename=v.sitename \n" +
-                "and e.startdate=v.eventstartdate\n" +
-                "and e.name in (select ev.name from event as ev "+eventNameSql1+"  "+descriptionSql1+" "+dateSql11+" ) \n" +
-                "and v.visitorusername='"+ UserInfo.username +"'\n" +
-                "and e.name=etv.eventname\n" +
-                "and e.sitename=etv.sitename\n" +
-                "and e.startdate=etv.eventstartdate\n" +
-                "group by v.eventname,v.sitename,v.eventstartdate;";
+        sql = "select e.name,e.sitename,e.price, (e.capacity-etv.totalvisit)as ticketremaining, etv.totalvisit,emv.myvisit, e.startdate,e.enddate,e.description\n" +
+                "from event as e\n" +
+                "left join eventtotalvisit as etv on e.name=etv.name and e.sitename=etv.sitename and e.startdate=etv.startdate\n" +
+                "left join eventmyvisit as emv on e.name=emv.name and e.sitename=emv.sitename and e.startdate=emv.startdate\n" +
+                "where "+eventNameSql+" "+descriptionSql+" "+siteSql +
+                dateSql1 +
+                " and e.price between '"+priceL+"' and '"+priceR+"' " +
+                " and etv.totalvisit between '"+visitL+"' and '"+visitR+"' " +
+                soldOutSql+
+                myVisitSql+
+                " group by e.name,e.sitename,e.startdate;";
 
         System.out.println(sql);
         ResultSet resultSet =  statement.executeQuery(sql);
--- src/sample/ExploreSite35.java
+++ src/sample/ExploreSite35.java
@@ -1,5 +1,6 @@
 package sample;
 
+import connection.ConnectionManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,10 +9,15 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import pojo.ExploreSiteRow35;
+import pojo.UserInfo;
 import tools.MyAlert;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.format.DateTimeFormatter;
 
 public class ExploreSite35 {
     public TableView table;
@@ -31,19 +37,38 @@ public class ExploreSite35 {
     public ComboBox cbName;
     public ComboBox cbOpenEveryday;
 
+    private static Connection conn;
+
     public void setLastFxml(String lastFxml) {
         this.lastFxml = lastFxml;
     }
 
-    public void initialize(){
+    public void initialize() throws SQLException {
         col1.setCellValueFactory(new PropertyValueFactory<>("siteName"));
         col2.setCellValueFactory(new PropertyValueFactory<>("eventCount"));
         col3.setCellValueFactory(new PropertyValueFactory<>("totalVisits"));
         col4.setCellValueFactory(new PropertyValueFactory<>("myVisits"));
         cbOpenEveryday.getItems().addAll(
+                "ALL",
                 "Yes",
                 "No"
         );
+
+        conn = ConnectionManager.getConn();
+        String sql = "select name from site";
+
+        Statement statement = conn.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        cbName.getItems().add("ALL");
+        while(resultSet.next()){
+            cbName.getItems().add(resultSet.getString("name"));
+        }
+
+        cbName.getSelectionModel().select(0);
+        cbOpenEveryday.getSelectionModel().select(0);
+        statement.close();
+
     }
 
     public void addElement(String siteName, int eventCount, int totalVisits, int myVisits) {
@@ -58,11 +83,96 @@ public class ExploreSite35 {
         stage.setScene(new Scene(root));
     }
 
-    public void btnFilter(ActionEvent actionEvent) {
-        ////test below
-        addElement("yoyoyo",1,5,7);
-        addElement("heyyyy",3,6,1);
-        ///test above
+    public void btnFilter(ActionEvent actionEvent) throws SQLException {
+
+
+        String formattedDate1 = "";
+        String formattedDate2 = "";
+        String dateTableSql1 = "";
+        if(datepicker1.getValue()!=null && datepicker2.getValue()!=null){
+            formattedDate1 = datepicker1.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            formattedDate2 = datepicker2.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            dateTableSql1 = " and (v.visitdate between '"+formattedDate1+"'and '"+formattedDate2+"') ";
+        }
+
+        String dateTableSql2 = "";
+        if(datepicker1.getValue()!=null && datepicker2.getValue()!=null){
+            dateTableSql2 = " and (v.visitdate between '"+formattedDate1+"' and '"+formattedDate2+"') ";
+        }
+
+
+        table.getItems().clear();
+        String sql = "CREATE  or replace view sitetotalvisit AS\n" +
+                "SELECT s.name,count(case when s.name=v.name "+dateTableSql1+" then 1 end) as totalvisit\n" +
+                "from site as s\n" +
+                "left join visit_site as v\n" +
+                "on s.name=v.name \n" +
+                "group by s.name;";
+        System.out.println(sql);
+        Statement statement = conn.createStatement();
+        statement.executeUpdate(sql);
+
+        sql = "CREATE  or replace view sitemyvisit AS\n" +
+                "SELECT s.name,count(case when s.name=v.name and v.visitorusername='"+UserInfo.username+"' "+dateTableSql2+" then 1 end) as myvisit\n" +
+                "from site as s\n" +
+                "left join visit_site as v\n" +
+                "on s.name=v.name \n" +
+                "group by s.name;";
+        statement.executeUpdate(sql);
+        System.out.println(sql);
+
+        sql = "CREATE  or replace view eventcount35 AS\n" +
+                "SELECT s.name,count(case when s.name=v.sitename and not (v.startdate>'"+formattedDate2+"' or v.enddate<'"+formattedDate1+"')then 1 end) as eventcount\n" +
+                "from site as s, event as v\n" +
+                "group by s.name;";
+        statement.executeUpdate(sql);
+        System.out.println(sql);
+
+
+        String siteSql = " 1=1 ";
+        if(!cbName.getValue().toString().equals("ALL")){
+            siteSql = " s.name='"+cbName.getValue().toString()+"' ";
+        }
+
+        String openEveryDaySql = "";
+        if(!cbOpenEveryday.getValue().toString().equals("ALL")){
+            openEveryDaySql = " and s.openeveryday='"+cbOpenEveryday.getValue().toString()+"' ";
+        }
+
+        String totalVisitL = "0", totalVisitR = Integer.MAX_VALUE+"";
+        if(tfTotalVisits1.getText().length()!=0)
+            totalVisitL = tfTotalVisits1.getText();
+        if(tfTotalVisits2.getText().length()!=0)
+            totalVisitR = tfTotalVisits2.getText();
+
+        String eventcountL = "0", eventcountR = Integer.MAX_VALUE+"";
+        if(tfEventCountRange1.getText().length()!=0)
+            eventcountL = tfTotalVisits1.getText();
+        if(tfEventCountRange1.getText().length()!=0)
+            eventcountR = tfTotalVisits2.getText();
+
+        String visitedSql = "";
+        if(!checkVisited.isSelected())
+            visitedSql = " and smv.myvisit= '0' " ;
+
+        sql = "select s.name,ec.eventcount,stv.totalvisit,smv.myvisit\n" +
+                "from site as s\n" +
+                "left join sitetotalvisit as stv on s.name=stv.name\n" +
+                "left join sitemyvisit  as smv on s.name=smv.name\n" +
+                "left join eventcount35 as  ec on s.name=ec.name\n" +
+                "where " +siteSql + openEveryDaySql +
+                "and stv.totalvisit between '"+totalVisitL+"' and '"+totalVisitR+"'\n" +
+                "and ec.eventcount between '"+eventcountL+"' and '"+eventcountR+"'\n" +
+                visitedSql;
+
+        System.out.println(sql);
+        ResultSet resultSet =  statement.executeQuery(sql);
+        while(resultSet.next()){
+            addElement(resultSet.getString(1),resultSet.getInt(2),resultSet.getInt(3),resultSet.getInt(4));
+        }
+        statement.close();
+
+
     }
 
     public void btnSiteDetail(ActionEvent actionEvent) throws IOException, SQLException {
